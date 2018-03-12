948ad4ab4d7a4046090f195bb38d22c994698810
AntonisMoul <AntonisMoul@users.noreply.github.com>
2018-03-12 19:48:02 +0200

    push

--- code/metrics/Correlation.py
+++ code/metrics/Correlation.py
@@ -68,12 +68,12 @@ class Correlation():
 		return self.__correlationMatrix
 		
 	
-	def getLowCorrelationSnps(self, b, c = 0, up = 100, down = 0):
+	def getLowCorrelationSnps(self, b=0.7, c = 0, up = 100, down = 0):
 		
 		return s.lowCorrelation(self.__correlationMatrix, b, c, up, down)
 		
-	def getHighCorrelationSnps(self, b, c = 0,up = 100, down = 0):
+	def getHighCorrelationSnps(self, b=0.7, c = 0,up = 100, down = 0):
 	
-		return s.highCorrelation(self.__correlationMatrix, b, up, down)
+		return s.highCorrelation(self.__correlationMatrix, b, c, up, down)
 	
 		
