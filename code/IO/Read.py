85417a5cff02fc3116832c27366c9cb63cd92cf2
Antonis <antoniskfl@gmail.com>
2018-04-13 21:46:14 +0300

    push

--- code/IO/Read.py
+++ code/IO/Read.py
@@ -25,7 +25,7 @@ class Read:
 
                 for line in f:
                     self.__numberOfPatients  += 1
-                    patients[line.split()[0].strip()] = PatientPhenotype(line.split()[0],line.split()[2])
+                    patients[line.split()[0].strip()] = PatientPhenotype(line.split()[0],line.split()[3])
                     
                 f.close()
 
