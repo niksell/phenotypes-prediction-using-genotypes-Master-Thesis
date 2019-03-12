abbebb070ec00777d2d054f33d2c356965ace305
gsalteris <nikselll@gmail.com>
2019-03-12 15:53:34 -0700

    try to add doc

--- js/init.js
+++ js/init.js
@@ -538,7 +538,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Freelancer", "UI/UX Designer", "Web Developer"],
+			strings: ["Data Scientist", "Software Engineer", "Web Developer"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
